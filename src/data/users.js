263cee9f33795ce340be9ca255f7250a71c61e65
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2021-12-27 19:44:41 -0300

    Layout inicial da lista usando o React Native Elements implementado

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: 'Eduardo Costa',
+    email: 'eduardo@email.com',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/38767339?v=4'
+  },
+  {
+    id: 2,
+    name: 'Vitória de Lima',
+    email: 'vitoria@email.com',
+    avatarUrl: 'https://static.diverseui.com/female-70.jpg'
+  },
+  {
+    id: 3,
+    name: 'Kim Hatsuki',
+    email: 'kim@email.com',
+    avatarUrl: 'https://static.diverseui.com/88b95197-fd1e-4e11-8793-2903a5cfd06e-10584053_10153749310922416_3125632463004974493_n.jpg'
+  },
+  {
+    id: 4,
+    name: 'Murilo Gentile',
+    email: 'mutilo@email.com',
+    avatarUrl: 'https://static.diverseui.com/male-36.jpg'
+  },
+]
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,59 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, FlatList, Alert } from 'react-native'
+import { Avatar, Button, Icon, ListItem } from 'react-native-elements'
+import users from '../data/users'
 
-export default function UserList(){
+export default function UserList({ navigation }) {
+
+  function editUser(user){
+    navigation.navigate('UserForm', user)
+  }
+  function removeUser(user){
+    Alert.alert('Excluir Usuário', 'Deseja excluir o susuário?', [
+      { text: 'Sim', onPress: () => console.warn('Removendo Usuário ' + user.id)},
+      { text: 'Não' }
+    ])
+  }
+
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem.Swipeable
+        onPress={() => navigation.navigate('UserForm')}
+        key={user.id}
+        bottomDivider
+        leftContent={
+          <Button
+            title="Editar"
+            icon={{ name: 'edit', color: 'white' }}
+            buttonStyle={{ minHeight: '100%', backgroundColor: '#3949ab' }}
+            onPress={ () => editUser(user) }
+          />
+        }
+        rightContent={
+          <Button
+            title="Remover"
+            icon={{ name: 'delete', color: 'white' }}
+            buttonStyle={{ minHeight: '100%', backgroundColor: '#d32f2f' }}
+            onPress={() => removeUser(user) }
+          />
+        }
+      >
+        <Avatar rounded source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem.Swipeable>
+    )
+  }
+
   return (
-    <Text>UserList</Text>
+    <View>
+      <FlatList
+        data={users}
+        keyExtractor={user => user.id.toString()}
+        renderItem={getUserItem}
+      />
+    </View>
   )
 }
